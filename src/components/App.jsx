56e6ab20b6e43be73855a8704658cb2239b9a608
marb <marb61a@gmail.com>
2016-03-23 11:22:50 +0000

    added constructor and defaults to app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,34 @@ import React, {component} from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends component{
-    
+    constructor(props){
+        super(props);
+        this.state = {
+            username: 'bradtraversy',
+			userData: [],
+			userRepos: [],
+			perPage: 5
+        };
+        }
+        
+        render(){
+            return(
+			<div>
+				{this.props.clientId}
+			</div>
+		);
+    }
 }
+
+App.propTypes ={
+    clientId: React.PropTypes.string,
+	clientSecret: React.PropTypes.string
+};
+
+App.defaultProps = {
+    clientId : "",
+    clientSecret : ""
+};
 
 export default App;
 
