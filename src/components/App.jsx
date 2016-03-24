f8cafa8c60cc565312b5d7114e9d289c58392b2b
marb <marb61a@gmail.com>
2016-03-24 14:44:36 +0000

    added get user data function o app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,10 +12,31 @@ class App extends component{
         };
         }
         
+        // Get user data from Github
+        getUserData(){
+            $.ajax({
+                url : 'https://api.github.com/users/'+this.state.username+'?client_id='+this.props.clientId+'&client_secret='+this.props.clientSecret,
+                dataType : 'json',
+                cache : 'false',
+                success : function(data){
+                    this.setState({userData: data});
+                    console.log(data);
+                }.bind(this),
+                error : function(xhr, status, err){
+				    this.setState({username: null});
+				    alert(err);
+			    }.bind(this)
+            });
+        }
+        
+        componentDidMount(){
+    		this.getUserData();
+    	}
+        
         render(){
             return(
 			<div>
-				{this.props.clientId}
+				<Profile userData = {this.state.userData} />
 			</div>
 		);
     }
