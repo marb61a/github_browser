d7759ef1fc7d351ba0f5e981e91ad9fdefad99f5
marb <marb61a@gmail.com>
2016-03-24 14:52:23 +0000

    added initial profile.ejs settings

---
+++ src/components/github/Profile.jsx
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class Profile extends Component{
+    render(){
+        return(
+            <div className="panel panel-default">
+                <div className="panel-heading">
+                    <h3 className="panel-title">
+                        {this.props.userData.name}
+                    </h3>
+                </div>
+                <div className="panel-body">
+                    <div class="row">
+                        <div className="col-md-4">
+			    		    <img src={this.props.userData.avatar_url} className="thumbnail" style={{width:"100%"}} />
+			    	    </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Profile;
