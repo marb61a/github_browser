97e766a15a5c5471f47bad1f60f3f292ae338c7c
marb <marb61a@gmail.com>
2016-03-27 12:41:35 +0000

    added search.jsx

---
+++ src/components/github/Search.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Search extends Component{
+    onSubmit(e){
+        e.preventDefault();
+		let username = this.refs.username.value.trim();
+		if(!username){
+			alert('Please enter a username');
+			return;
+		}
+		this.props.onFormSubmit(username);
+		this.refs.username.value = '';
+    }
+}
+
+export default Search;
